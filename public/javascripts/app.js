6fc8f868e7c725dae5e548414f0fa186550d98bd
Jason Godson <jason_c_g@hotmail.com>
2016-03-02 22:04:08 -0600

    Added timestamps to messages and added styles

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,8 +2,10 @@ var socket = io();
 
 socket.on('connect', function () {
 	socket.on('message', function(message) {
-		$('#received').append("<p class='message received'>" + message.text + "</p>");
-		console.log(message.text);
+		message.timestamp = moment(message.timestamp);
+		$('#received').append("<p class='message received'><strong>" + message.timestamp.local().format('h:mm a')
+			+ "</strong>: "+ message.text + "</p>");
+		$('#sent').append("<p class='message empty'>" + message.text + "</p>");
 	});
 });
 
@@ -12,12 +14,16 @@ var $form = jQuery('#message-form');
 var messageBox = $form.find('input[name=message]');
 $form.on('submit', function(event) {
 	event.preventDefault();
+	var timestamp = moment();
 	var sentMessage = messageBox.val();
 	if (sentMessage !== "") {
 		socket.emit('message', {
-			text: sentMessage
+			text: sentMessage,
+			timestamp: timestamp
 		});
+		$('#received').append("<p class='message empty'>" + sentMessage + "</p>");
+		$('#sent').append("<p class='message sent'><strong>" + timestamp.local().format('h:mm a') + "</strong>: "
+			+ sentMessage + "</p>");
+		messageBox.val("");
 	}
-	$('#sent').append("<p class='message sent'>" + sentMessage + "</p>");
-	messageBox.val("");
 });
--- server.js
+++ server.js
@@ -5,12 +5,14 @@ var express = require('express');
 var server = express();
 var http = require('http').Server(server);
 var io = require('socket.io')(http);
+var moment = require('moment');
+
+var now = moment();
 
 io.on('connection', function(socket) {
 	console.log("User connected via socket.io");
 	
 	socket.on('message', function(message) {
-		console.log("Got message");
 		socket.broadcast.emit('message', message);
 	});
 	

1d40971157233dd7fc7be9c5fd0b60351fb4ca51
Jason Godson <jason_c_g@hotmail.com>
2016-03-03 07:39:23 -0600

    Added names

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,11 +1,14 @@
 var socket = io();
+var params = getQueryParams() || {name : "Anonymous"};
+$sent = $('#sent');
+$received = $('#received');
 
 socket.on('connect', function () {
 	socket.on('message', function(message) {
 		message.timestamp = moment(message.timestamp);
-		$('#received').append("<p class='message received'><strong>" + message.timestamp.local().format('h:mm a')
-			+ "</strong>: "+ message.text + "</p>");
-		$('#sent').append("<p class='message empty'>" + message.text + "</p>");
+		$received.append("<p class='message received'><strong>" + message.name +"</strong> @ <strong>" 
+		+ message.timestamp.local().format('h:mm a') + "</strong>:<br>"+ message.text + "</p>");
+		$sent.append("<p class='message empty'>\n" + message.text + "</p>");
 	});
 });
 
@@ -18,11 +21,12 @@ $form.on('submit', function(event) {
 	var sentMessage = messageBox.val();
 	if (sentMessage !== "") {
 		socket.emit('message', {
+			name: params.name,
 			text: sentMessage,
 			timestamp: timestamp
 		});
-		$('#received').append("<p class='message empty'>" + sentMessage + "</p>");
-		$('#sent').append("<p class='message sent'><strong>" + timestamp.local().format('h:mm a') + "</strong>: "
+		$received.append("<p class='message empty'>" + sentMessage + "</p>");
+		$sent.append("<p class='message sent'><strong>" + timestamp.local().format('h:mm a') + "</strong>: "
 			+ sentMessage + "</p>");
 		messageBox.val("");
 	}
---
+++ public/javascripts/queryparams.js
@@ -0,0 +1,14 @@
+function getQueryParams() {
+	var url = window.location.href;
+	if (url.indexOf('?') > -1) {
+		var queries = url.split('?');
+		queries = queries[1].split('&');
+		var queryObj = {};
+		queries.forEach(function(query) {
+			query = query.split('=');
+			queryObj[decodeURIComponent(query[0])] = decodeURIComponent(query[1]);
+		});
+		return queryObj;
+	}
+	return undefined;
+}
